098cf7b601c8b4d0aec9b01553d14651cd74e8a1
sarroutbi <sarroutbi@yahoo.es>
2016-10-06 19:16:56 +0200

    Adding opos list list reincorporations

--- scripts/utils/check_opos_lists.sh
+++ scripts/utils/check_opos_lists.sh
@@ -6,6 +6,7 @@ TMP_LAST_INTERIM_CALLED=/tmp/ultimos_interinos_citados.pdf
 TMP_NEXT_INTERIM_CALLED=/tmp/siguientes_interinos_citados.pdf
 TMP_LAST_VOLUNTEER_CALLED=/tmp/ultimos_voluntarios_citados.pdf
 TMP_NEXT_VOLUNTEER_CALLED=/tmp/siguientes_voluntarios_citados.pdf
+TMP_INC_CALLED=/tmp/incorporados_lista.pdf
 
 # Function to retrieve the file to download. Parameters:
 # 1 - Type of list (i.e.:interinos, voluntarios)
@@ -28,7 +29,7 @@ function get_list_date()
 # 1: Invalid Date 
 function is_valid_date()
 {
-  echo $1 | egrep -E "[0-9]{1,2}/[0-9]{2}/[0-9]{4}" > /dev/null
+  echo $1 | egrep -E "[0-9]{2}/[0-9]{2}/[0-9]{4}" > /dev/null
   echo $?
 }
 
@@ -38,7 +39,8 @@ function retrieve_files()
   get_file "Últimos interinos citados" ${TMP_LAST_INTERIM_CALLED}  
   get_file "Próximos interinos" ${TMP_NEXT_INTERIM_CALLED}  
   get_file "últimos interinos citados para puestos de carácter voluntario" ${TMP_LAST_VOLUNTEER_CALLED}  
-  get_file "Interinos citados para puestos de carácter voluntario" ${TMP_NEXT_VOLUNTEER_CALLED}  
+  get_file "Interinos citados para puestos de carácter voluntario" ${TMP_NEXT_VOLUNTEER_CALLED}
+  get_file "Incorporados a la lista en su orden" "${TMP_INC_CALLED}"
 }
 
 LAST_DATE=$(get_list_date)
@@ -58,7 +60,8 @@ do
   if [ "${NOW_DATE}" != "${LAST_DATE}" ] && [ ${NOW_INVALID_DATE} -eq 0 ] && [ ${LAST_INVALID_DATE} -eq 0 ];
   then
     retrieve_files
-    /usr/local/src/sendEmail/sendEmail -f from@mail.com -t to@mail.com -u "Actualizacion de listas de interinos" -u "Actualizacion de listas de interinos" -m "Se ha detectado una actualizacion en la lista de interinos. Fecha antigua:${LAST_DATE}, Fecha nueva:${NOW_DATE}.\nConsulte el enlace:\n${LINK}" -v -xu user -xp password -a "${TMP_LAST_INTERIM_CALLED}" -a "${TMP_NEXT_INTERIM_CALLED}" -a "${TMP_LAST_VOLUNTEER_CALLED}" -a "${TMP_NEXT_VOLUNTEER_CALLED}"
+    /usr/local/src/sendEmail/sendEmail -f from@mail.com -t to@mail.com -u "Actualizacion de listas de interinos" -u "Actualizacion de listas de interinos" -m "Se ha detectado una actualizacion en la lista de interinos. Fecha antigua:${LAST_DATE}, Fecha nueva:${NOW_DATE}.\nConsulte el enlace:\n${LINK}" -v -xu user -xp password -a "${TMP_LAST_INTERIM_CALLED}" -a "${TMP_NEXT_INTERIM_CALLED}" -a "${TMP_LAST_VOLUNTEER_CALLED}" -a "${TMP_NEXT_VOLUNTEER_CALLED}" -a "${TMP_INC_CALLED}"
+
     LAST_DATE=${NOW_DATE}
   fi
   sleep ${CHECK_LOOP}
