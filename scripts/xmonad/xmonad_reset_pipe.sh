d3c0251915dc93c079914b1b341b045a449cefee
sarroutbi <sarroutbi@yahoo.es>
2016-12-08 11:36:49 +0100

    adding changes to avoid ubuntu 16.04 xmonad+xmobar freeze

--- scripts/xmonad/xmonad_reset_pipe.sh
+++ scripts/xmonad/xmonad_reset_pipe.sh
@@ -7,11 +7,15 @@ then
 fi
 pid=$(pidof xmonad-x86_64-linux)
 echo "PID:${pid}"
+xmobar_pid=$(pidof xmobar)
+echo "XMOBAR PID:${xmobar_pid}"
 
 while true;
 do
   pipe=$(ls -1 /proc/${pid}/fd/ | sort -n | tail -1)
+  xmobar_pipe=$(ls -1 /proc/${xmobar_pid}/fd/ | sort -n | tail -1)
   cat /proc/${pid}/fd/${pipe} &
+  cat /proc/${xmobar_pid}/fd/${xmobar_pipe} &
   pid_cat=$(pidof cat)
   sleep 1
   test -z "${pid_cat}" && kill -9 ${pid_cat} 2>/dev/null 1>/dev/null 
@@ -21,4 +25,5 @@ do
   fi
   sleep 1
   killall -9 cat
+  pkill -CONT xmonad-x86_64-linux
 done
