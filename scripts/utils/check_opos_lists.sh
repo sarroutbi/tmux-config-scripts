e531ca9cf89ec170364ee419d724c447c52feb3a
sarroutbi <sarroutbi@yahoo.es>
2017-01-11 10:39:11 +0100

    Updating parsing

--- scripts/utils/check_opos_lists.sh
+++ scripts/utils/check_opos_lists.sh
@@ -53,7 +53,7 @@ function retrieve_files()
   get_file "${LINK}" "${GLOBAL_LINK}" "Próximos interinos" ${TMP_NEXT_INTERIM_CALLED}
   get_file "${LINK}" "${GLOBAL_LINK}" "últimos interinos citados para puestos de carácter voluntario" ${TMP_LAST_VOLUNTEER_CALLED}
   get_file "${LINK}" "${GLOBAL_LINK}" "Interinos citados para puestos de carácter voluntario" ${TMP_NEXT_VOLUNTEER_CALLED}
-  get_file "${LINK}" "${GLOBAL_LINK}" "Incorporados a la lista en su orden" "${TMP_INC_CALLED}"
+  get_file "${LINK}" "${GLOBAL_LINK}" "Incorporados a las listas en su orden" "${TMP_INC_CALLED}"
 }
 
 function retrieve_pri_files()
@@ -63,7 +63,7 @@ function retrieve_pri_files()
   get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "Próximos interinos" ${TMP_PRI_NEXT_INTERIM_CALLED}
   get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "últimos interinos citados para puestos de carácter voluntario" ${TMP_PRI_LAST_VOLUNTEER_CALLED}
   get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "Interinos citados para puestos de carácter voluntario" ${TMP_PRI_NEXT_VOLUNTEER_CALLED}
-  get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "Incorporados a la lista en su orden" ${TMP_PRI_INC_CALLED}
+  get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "Incorporados a las listas en su orden" ${TMP_PRI_INC_CALLED}
 }
 
 LAST_DATE=$(get_list_date)
