0014b93fe8594637fa90237a8b1a1aac67cf49fa
sarroutbi <sarroutbi@yahoo.es>
2017-09-14 11:44:24 +0200

    link check

--- scripts/utils/check_opos_lists.sh
+++ scripts/utils/check_opos_lists.sh
@@ -61,7 +61,7 @@ function retrieve_pri_files()
   echo "Retrieving files ..."
   get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "Últimos interinos citados" ${TMP_PRI_LAST_INTERIM_CALLED}
   get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "Próximos interinos" ${TMP_PRI_NEXT_INTERIM_CALLED}
-  get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "Puntuaciones de últimos participantes citados para cubrir puestos de carácter voluntario" ${TMP_PRI_LAST_VOLUNTEER_CALLED}
+  get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "Interinos citados para puestos de carácter voluntario" ${TMP_PRI_LAST_VOLUNTEER_CALLED}
   get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "últimos interinos citados para puestos de carácter voluntario" ${TMP_PRI_NEXT_VOLUNTEER_CALLED}
   get_file "${PRIMARY_LINK}" "${GLOBAL_LINK}" "Incorporados a las listas en su orden" ${TMP_PRI_INC_CALLED}
 }
