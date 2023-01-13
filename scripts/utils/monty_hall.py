4b5ab4bc758a0a63c3ae472d8f29f8f2d1c6a343
Sergio Arroutbi <sarroutb@redhat.com>
2023-01-13 11:05:32 +0100

    Fix most important pylint reported errors
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- scripts/utils/monty_hall.py
+++ scripts/utils/monty_hall.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-# Copyright (c) 2017, Sergio Arroutbi Braojos <sarroutbi (at) yahoo.es>
-# 
+# Copyright (c) 2017, Sergio Arroutbi Braojos <sarroutbi (at) redhat.com>
+#
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 # WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 # MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -15,7 +15,7 @@
 """
 Program to test Monty Hall paradigm
 Usage:
-${0} [-c: change user] [-d: number of doors, 3 by default] [-i: number of iterations, 1000 by default]
+${0} [-c: change door] [-d: number of doors, 3 by default] [-i: iterations, 10000 by default]
 """
 import argparse
 import random
@@ -28,119 +28,139 @@ DEFAULT_CHANGE=0
 DEFAULT_ITERATIONS=10000
 
 def str2bool(value):
-  return value.lower() in ("yes", "true", "t", "1")
+    return value.lower() in ("yes", "true", "t", "1")
 
-class DOOR_CONTENT(Enum):
-  GOAT  = 1
-  CAR   = 2
-  NONE = 3
+class DoorContent(Enum):
+    GOAT  = 1
+    CAR   = 2
+    NONE = 3
 
-class Result(object):
-  """
-  Representation of a result
-  
-  Attributes:
-  - selected (door selected by user)
-  - content (goat/cat)
-  """
-  def __init__(self):
-    self.win = 0
-    self.loose = 0
-  def __str__(self):
-    return str("-----------------------------" + "\n"
-               "Wins:"   + str(self.win) + " " +
-               "Looses:" + str(self.loose) + "\n" +
-               "-----------------------------")
-  def add_win(self):
-    self.win += 1
-  def add_loose(self):
-    self.loose += 1
+class Result:
+    """
+    Representation of a result
 
-class Door(object):
-  """
-  Representation of a door
+    Attributes:
+    - selected (door selected by user)
+    - content (goat/cat)
+    """
+    def __init__(self):
+        self.win = 0
+        self.loose = 0
+    def __str__(self):
+        return str("-----------------------------" + "\n"
+                   "Wins:"   + str(self.win) + " " +
+                   "Looses:" + str(self.loose) + "\n" +
+                   "-----------------------------")
+    def add_win(self):
+        self.win += 1
+    def add_loose(self):
+        self.loose += 1
 
-  Attributes:
-  - selected (door selected by user)
-  - content (goat/cat)
-  - opened
-  """
-  def __init__(self, content_type = DOOR_CONTENT.NONE):
-    self.content = content_type
-    self.selected = False
-    self.opened = False
-  def is_winning_door(self):
-    return self.selected and DOOR_CONTENT.CAR == self.content
-  def open(self):
-    self.open = True
-  def __str__(self):
-    return "[%s]" % ("G" if self.content == DOOR_CONTENT.GOAT
-                     else "C")
+class Door:
+    """
+    Representation of a door
+
+    Attributes:
+    - selected (door selected by user)
+    - content (goat/cat)
+    - opened
+    """
+    def __init__(self, content_type = DoorContent.NONE):
+        self.content = content_type
+        self.selected = False
+        self.opened = False
+    def is_winning_door(self):
+        return self.selected and DoorContent.CAR == self.content
+    def open(self):
+        self.opened = True
+    def __str__(self):
+        return '[%s%s%s]' % ("G" if self.content == DoorContent.GOAT
+                             else "C", "o" if self.opened else "c",
+                             "s" if self.selected else "n")
+def print_doors(doors):
+    for door in doors:
+        print(door, end="")
+    print()
 
 def user_wins(doors):
-  for door in doors:
-    if door.is_winning_door():
-      return True
+    for door in doors:
+        if door.is_winning_door():
+            return True
+    return False
 
 def create_doors(args):
-  doors = []
-  car_position = random.randint(0, args.d-1)
-  for x in range(0, args.d):
-    if car_position == x:
-      door = Door(DOOR_CONTENT.CAR)
-    else:
-      door = Door(DOOR_CONTENT.GOAT)
-    doors.append(door)
-  return doors
+    doors = []
+    car_position = random.randint(0, args.d-1)
+    for udoor in range(0, args.d):
+        if car_position == udoor:
+            door = Door(DoorContent.CAR)
+        else:
+            door = Door(DoorContent.GOAT)
+        doors.append(door)
+    return doors
 
-def open_goat_door(args, doors):
-  for x in range(0, len(doors)):
-    if doors[x].content == DOOR_CONTENT.GOAT:
-      doors[x].open()
-      return doors
+def open_goat_door(doors):
+    for door in doors:
+        if door.content is DoorContent.GOAT and door.selected is False \
+           and door.opened is False:
+            door.open()
+            return doors
+    return doors
 
 def user_selects_door(doors):
-  user_selection = random.randint(0, len(doors)-1)
-  doors[user_selection].selected = True
+    user_selection = random.randint(0, len(doors)-1)
+    doors[user_selection].selected = True
 
 def change_user_selection(doors):
-  while True:
-    selection = random.randint(0, len(doors)-1)
+    user_made_selection = False
     for door_number in range(0, len(doors)):
-      if door_number == selection and not doors[door_number].opened and not doors[door_number].selected:
-        doors[door_number].selected = True
-        return True
+        if doors[door_number].selected:
+            previous_selected = door_number
+    while not user_made_selection:
+        selection = random.randint(0, len(doors)-1)
+        for door_number in range(0, len(doors)):
+            if door_number == selection and not doors[door_number].opened \
+               and not doors[door_number].selected:
+                doors[door_number].selected = True
+                user_made_selection = True
+                doors[previous_selected].selected = False
+        if user_made_selection:
+            return
 
 def parse_args():
-  parser = argparse.ArgumentParser(os.path.basename(sys.argv[0]) + ' program tests Monty Hall paradigm')
-  parser.add_argument('-d', help='number of doors', type=int, default=DEFAULT_DOORS_AMOUNT)
-  parser.add_argument('-i', help='number of iterations (times to play)', type=int, default=DEFAULT_ITERATIONS)
-  parser.add_argument('-c', help='specify just in case that user changes the door', type=int, default=DEFAULT_CHANGE)
-  return parser.parse_args()
+    parser = argparse.ArgumentParser(os.path.basename(sys.argv[0]) + \
+                                     ' program tests Monty Hall paradigm')
+    parser.add_argument('-d', help='number of doors',
+                        type=int, default=DEFAULT_DOORS_AMOUNT)
+    parser.add_argument('-i', help='number of iterations (times to play)',
+                        type=int, default=DEFAULT_ITERATIONS)
+    parser.add_argument('-c', help='specify just in case that user changes the door',
+                        type=int, default=DEFAULT_CHANGE)
+    return parser.parse_args()
 
 def calculate_results(args, doors, results):
-  open_goat_door(args, doors)
-  if args.c == 1:
-      change_user_selection(doors)
-  if user_wins(doors):
-    results.add_win()
-  else:
-    results.add_loose()
+    open_goat_door(doors)
+    if args.c == 1:
+        change_user_selection(doors)
+    if user_wins(doors):
+        results.add_win()
+    else:
+        results.add_loose()
 
 def iterations(args):
-  result = Result()
+    result = Result()
 
-  # For each iteration (number of iteration is a parameter)
-  for iteration in range(0, args.i):
-    # Create the doors (number of doors is other parameter)
-    doors = create_doors(args)
-    # User selects door randomly, and changes door or not (door change is a parameter)
-    user_selects_door(doors)
-    # Calculate and store result
-    calculate_results(args, doors, result)
-    # end iterations
-  print(result)
-    
+    # For each iteration (number of iteration is a parameter)
+    for _ in range(0, args.i):
+        # Create the doors (number of doors is other parameter)
+        doors = create_doors(args)
+        # User selects door randomly, and changes door or not (door change is a parameter)
+        user_selects_door(doors)
+        # Calculate and store result
+        calculate_results(args, doors, result)
+        # end iterations
+    print(result)
+
 def main():
     iterations(parse_args())
 
