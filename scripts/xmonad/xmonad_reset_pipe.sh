7b831a5c35c95156ea9c3e288f0cbef8f6a0f6f8
Sergio Arroutbi <sarroutb@redhat.com>
2023-03-15 10:17:58 +0100

    Fix issues reported by shellcheck (#7)
    Resolves: #6
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- scripts/xmonad/xmonad_reset_pipe.sh
+++ scripts/xmonad/xmonad_reset_pipe.sh
@@ -12,22 +12,22 @@ echo "XMOBAR PID:${xmobar_pid}"
 
 while true;
 do
-  xmonad_max_pipe=$(ls -1 /proc/${xmonad_pid}/fd/ | sort -n | tail -1)
-  xmobar_max_pipe=$(ls -1 /proc/${xmobar_pid}/fd/ | sort -n | tail -1)
+  xmonad_max_pipe=$(ls -1 /proc/"${xmonad_pid}"/fd/ | sort -n | tail -1)
+  xmobar_max_pipe=$(ls -1 /proc/"${xmobar_pid}"/fd/ | sort -n | tail -1)
   echo "XMONAD MAX PIPE:${xmonad_max_pipe}"
   echo "XMOBAR MAX PIPE:${xmobar_max_pipe}"
-  for xmonad_fd in $(seq 0 $xmonad_max_pipe);
+  for xmonad_fd in $(seq 0 "${xmonad_max_pipe}");
   do
-    cat /proc/${xmonad_pid}/fd/${xmonad_fd} &
+    cat /proc/"${xmonad_pid}"/fd/"${xmonad_fd}" &
   done
-  for xmobar_fd in $(seq 0 $xmobar_max_pipe);
+  for xmobar_fd in $(seq 0 "${xmobar_max_pipe}");
   do
-    cat /proc/${xmobar_pid}/fd/${xmobar_fd} &
+    cat /proc/"${xmobar_pid}"/fd/"${xmobar_fd}" &
   done
   pid_cat=$(pidof cat)
   sleep 1
-  test -z "${pid_cat}" && kill -9 ${pid_cat} 2>/dev/null 1>/dev/null 
-  if [ $MODE_LOOP -eq 0 ];
+  test -z "${pid_cat}" && kill -9 "${pid_cat}" 2>/dev/null 1>/dev/null
+  if [ ${MODE_LOOP} -eq 0 ];
   then
     break;
   fi
