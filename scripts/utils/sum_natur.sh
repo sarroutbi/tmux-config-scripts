ff0933bfa80176f617e1771a065d0844fe11d81a
Sergio Arroutbi <sarroutb@redhat.com>
2023-04-26 12:15:49 +0200

    Fix issues reported by shellcheck (#18)
    Resolves: #17
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- scripts/utils/sum_natur.sh
+++ scripts/utils/sum_natur.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 #
-test -z ${1} && echo "Especifica el numero hasta el que sumar" && exit 1
-echo ${1} | egrep -E "[0-9]{1,}" 2>&1 > /dev/null || (echo "Especifica un numero entero" && exit 2)
+test -z "${1}" && echo "Especifica el numero hasta el que sumar" && exit 1
+echo "${1}" | grep -E "[0-9]{1,}" 2>/dev/null 1>/dev/null || {
+  echo "Especifica un numero entero"
+  exit 2
+}
 
-let big_number=$1
-let num=1
-let sum=0
+big_number=$1
+num=1
+sum=0
 
-while [ $num -le $big_number ];
+while [ $num -le "${big_number}" ];
 do
-  let sum=${num}+${sum}
-  let num=${num}+1 
+  ((sum=num+sum))
+  ((num=num+1))
 done
 
-echo "La suma es : ${sum}"
+echo "La suma es:${sum}"
 
