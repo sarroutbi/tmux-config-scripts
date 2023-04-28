050b0605aa42bfb21a51b9d8726f3733afeb2721
Sergio Arroutbi <sarroutb@redhat.com>
2023-04-28 19:08:00 +0200

    Fix errors reported by shellcheck (#22)
    Resolves: #21
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- scripts/utils/eval.sh
+++ scripts/utils/eval.sh
@@ -17,9 +17,12 @@ FILE_VAR2="/tmp/2"
 
 array_vars=(VAR1 VAR2)
 
+echo "FILE_VAR1:${FILE_VAR1}"
+echo "FILE_VAR2:${FILE_VAR2}"
+
 for ((ix=0; ix<${#array_vars[*]}; ix++)) 
 do
   eval MI_VARIABLE="\$FILE_${array_vars[$ix]}"
-  echo "Elemento [$ix]: ${array_vars[$ix]}, Fichero: ${MI_VARIABLE}"
+  echo "Elemento [$ix]:${array_vars[$ix]}, Fichero:${MI_VARIABLE}"
 done
 
