105ba856e6cd1cee2b8f137a951d7a234414302c
Sergio Arroutbi <sarroutb@redhat.com>
2024-10-03 12:58:39 +0200

    Fix tmux test script (#76)
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- config/tmuxes/tmux_test.sh
+++ config/tmuxes/tmux_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tmux new -s test_session  -d
-tmux source-file ~/tmuxes/.tmux.common.conf
+tmux source-file ~/tmuxes/tmux.common.conf
 tmux rename-window 'zero'
 tmux new-window -t test_session:1 -n 'two'
 tmux attach-session
