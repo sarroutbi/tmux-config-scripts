fca78712462b1ee14995461cbc2019f48866de36
Sergio Arroutbi <sarroutb@redhat.com>
2025-11-24 10:59:56 +0100

    Add clevis trustee pin tmux configuration file
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ tmux_clevis_trustee.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# Copyright (c) 2025, Sergio Arroutbi Braojos <sarroutbi (at) redhat.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+SESSION_NAME="CLEVIS_TRUSTEE"
+test -z "${1}" 2>/dev/null && {
+	echo
+	echo "Must provide an initial directory"
+	echo
+	exit 1
+}
+cd "${1}" 2>/dev/null || {
+	echo
+	echo "Directory not found"
+	echo
+	exit 1
+}
+test -z "${2}" 2>/dev/null || {
+	SESSION_NAME="${2}"
+}
+tmux new -s "${SESSION_NAME}" -d
+tmux source-file ~/tmuxes/tmux.common.conf
+tmux rename-window 'claude'
+tmux new-window -t "${SESSION_NAME}":1 -n 'compile'
+tmux new-window -t "${SESSION_NAME}":2 -n 'edit'
+tmux new-window -t "${SESSION_NAME}":3 -n 'tang'
+tmux new-window -t "${SESSION_NAME}":4 -n 'clevis'
+tmux new-window -t "${SESSION_NAME}":5 -n 'trustee'
+tmux new-window -t "${SESSION_NAME}":6 -n 'misc'
+tmux new-window -t "${SESSION_NAME}":7 -n 'DOC'
+tmux new-window -t "${SESSION_NAME}":8 -n 'tests'
+tmux new-window -t "${SESSION_NAME}":9 -n 'tmt'
+tmux attach -t "${SESSION_NAME}"
